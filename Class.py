a9152f0224aec2e976df16ba661d0f294075e317
PotapovAndr <potapovandr@gmail.com>
2024-10-16 21:20:31 +0900

    Выставление оценок

--- Class.py
+++ Class.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            print(f"Не могу оценить лектора {lecturer.name} {lecturer.surname} по курсу {course}.")
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -14,21 +23,34 @@ class Mentor:
         self.courses_attached = []
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
-    pass
+    def rate_student(self, student, course, grade):
+        if course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            print(f"Не могу оценить студента {student.name} {student.surname} по курсу {course}.")
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.finished_courses += ['Git']
 best_student.courses_in_progress += ['Python']
-best_student.grades['Git'] = [10, 10, 10, 10, 10]
-best_student.grades['Python'] = [10, 10]
 
-print(best_student.finished_courses)
-print(best_student.courses_in_progress)
-print(best_student.grades)
+cool_lecturer = Lecturer('Some', 'Buddy')
+cool_lecturer.courses_attached += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-print(cool_mentor.courses_attached)
+best_student.rate_lecturer(cool_lecturer, 'Python', 9)
+
+print(f'Оценки лектора {cool_lecturer.name} {cool_lecturer.surname}: {cool_lecturer.grades}')
+
+cool_reviewer = Reviewer('Ann', 'Smith')
+cool_reviewer.courses_attached += ['Python']
+
+cool_reviewer.rate_student(best_student, 'Python', 10)
+
+print(f'Оценки студента {best_student.name} {best_student.surname}: {best_student.grades}')
